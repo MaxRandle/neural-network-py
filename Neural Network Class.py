1ec809e95df48615de81e6c231d6d5b55c036405
MaxRandle <31084618+MaxRandle@users.noreply.github.com>
2019-05-10 13:40:58 +1200

    Update Neural Network Class.py
    bug fix

--- Neural Network Class.py
+++ Neural Network Class.py
@@ -52,20 +52,26 @@ class NN:
         return a
     
     ff = feedForward
-
+        
     def backPropogate(self, y, a):
         """
         y is the expected output matrix
         a is the list of activation matricies
         """
+        #output layer is handled differently to hidden layers
         error = y - a[-1]
+        gradient = np.multiply(error, self.dg(a[-1]))
+        delta = a[-2].T * gradient
+        self.weights[-1] += delta * self.alpha
+        self.biases[-1] += gradient * self.alpha
+
         ls = len(self.weights)
-        for n in range(1, ls)[::-1]:
-            gradient = np.multiply(self.dg(a[n]), error) * self.alpha
-            delta = gradient * self.weights[n].T
-            self.weights[n] = self.weights[n] + delta
-            self.biases[n] = self.biases[n] + gradient
-            error =  error * self.weights[n].T
+        for n in range(1, ls-1)[::-1]:
+            error = gradient * self.weights[n+1].T
+            gradient = np.multiply(error, self.dg(a[n]))
+            delta = a[n-1].T * gradient
+            self.weights[n] += delta * self.alpha
+            self.biases[n] += gradient * self.alpha
             
     bp = backPropogate
 
@@ -76,61 +82,21 @@ class NN:
         epochs is the number of times the network should train over the given training data.
         """
         for n in range(epochs):
+            y_error = []
             activations = []
             for i in range(len(train_x)):
                 activations = self.feedForward(train_x[i])
+                y_error = train_y[i] - activations[-1]
                 self.backPropogate(train_y[i], activations)
                 
-            if (n == epochs//4) or (n == 2*epochs//4) or (n == 3*epochs//4) or (n == epochs-1):
+
+            
+            #if (n == 1) or (n == epochs//4) or (n == 2*epochs//4) or (n == 3*epochs//4) or (n == epochs-1):
             #if (n % epochs//4) == 0:
-                print("Training loss: " + str(np.mean(np.abs(train_y[-1] - activations[-1]))))
+            if (n % 250 == 0):
+                print("Training loss: " + str(np.mean(np.square(y_error))))
+                #print("Training loss: " + str(np.mean(np.abs(train_y[-1] - activations[-1]))))
                 
-
-"""
-
----Example Neural Network---
-
- L0                L1                              L2                              L3
-         [        [b1]             ]     [        [b2]             ]     [        [b3]             ]
-         [         +               ]     [         +               ]     [         +               ]
-[a0]     [ [w1]   [z1]        [a1] ]     [ [w2]   [z2]        [a2] ]     [ [w3]   [z3]        [a3] ]
-[a0]  *  [ [w1] * [z1] g(z)-> [a1] ]  *  [ [w2] * [z2] g(z)-> [a2] ]  *  [ [w3] * [z3] g(z)-> [a3] ]
-[a0]     [ [w1]   [z1]        [a1] ]     [ [w2]   [z2]        [a2] ]     [ [w3]   [z3]        [a3] ]
-
----Example code for learning---
-
-data set {
-    [x] [y]
-    [x] [y]
-    [x] [y]
-}
-
-alpha = 0.1 //learning rate
-
-func feedforward(x) {
-    a = new array[4]
-    a[0] = x
-    for n in (1 -> 4) {
-        a[n] = g(a[n-1] * w[n] + b[n])
-    }
-    return a
-}
-
-a = feedforward(x)
-
-func backpropogate(a) {
-    error = y - a[-1]
-    for n in (3 -> 0) {
-        gradient = g'(a[n]) .* error
-        delta = gradient * w[n].T
-        w[n] = w[n] + delta * alpha
-        b[n] = b[n] + gradient * alpha
-        error = error * w[n].T
-    }
-}
-
-"""
-
 
 
 #example training data to solve the XOR problem
@@ -153,18 +119,9 @@ x = train_x
 y = train_y
 
 nn = NN(2)
+nn.add(3)
+nn.add(4)
 nn.add(2)
-nn.add(2)
-
-print(nn)
-
-nn.train(x, y, 10000)
-
-print(nn)
 
 
-
-
-
-
-
+nn.train(x, y, 15000 +1 )
