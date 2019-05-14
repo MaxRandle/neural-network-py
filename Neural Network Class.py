f6da0cbe1311c54d416d84c07e64bda1050f2e08
MaxRandle <31084618+MaxRandle@users.noreply.github.com>
2019-05-14 12:52:08 +1200

    code cleanup and added option for relu activation

--- Neural Network Class.py
+++ Neural Network Class.py
@@ -1,80 +1,86 @@
-#NN class by Max Randle
+#Neural Network class by Max Randle
 
 import numpy as np
 
 class NN:
     def __init__(self, number_of_inputs):
-        """number_of_inputs is the number of input neurons"""
-        self.weights    = [np.identity(number_of_inputs)]
-        self.biases     = [np.zeros((1, number_of_inputs))]
-        self.numInp = number_of_inputs
-        self.alpha = 0.1 #learning rate
+        self.activation = ["relu"]
+        self.w = [np.matrix(np.identity(number_of_inputs))]
+        self.b = [np.matrix(np.zeros((1, number_of_inputs)))]
+        self.alpha = 0.05 #learning rate
 
-    def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+    def sig(self, z):
+        return np.matrix(np.vectorize(lambda x: 1 / (1 + np.exp(-x)))(z))
 
-    def sigmoidDerivative(self, z):
-        return z * (1 - z)
-    
-    def g(self, z):
-        return np.matrix(np.vectorize(self.sigmoid)(z))
+    def sigDeriv(self, z):
+        return np.matrix(np.vectorize(lambda x: x * (1-x))(z))
 
-    def dg(self, z):
-        return np.matrix(np.vectorize(self.sigmoidDerivative)(z))
+    def relu(self, z):
+        return np.matrix(np.vectorize(lambda x: x if x > 0 else 0)(z))
 
-    def addDenseLayer(self, number_of_nodes):
-        """adds a fully connected layer into the network. Initialized with random weights."""
-        #Structure of each weight matrix is such that:
-        #The number of ROWS is equal to the number of nodes in the PREVIOUS layer
-        #The number of COLUMNS is equal to the number of nodes in the CURRENT layer
-
-        self.weights    += [np.matrix(2 * np.random.random((self.weights[-1].shape[1], number_of_nodes)) - 1)]
-        self.biases     += [np.matrix(np.ones((1, number_of_nodes)))]
-
-    add = addDenseLayer
+    def reluDeriv(self, z):
+        return np.matrix(np.vectorize(lambda x: 1 if x > 0 else 0)(z))
 
     def __repr__(self):
         out = "Layer View\n"
-        for i in range(len(self.weights)):
-            out += "Layer {} Weight ".format(i) + str(self.weights[i].shape) + "\n" + str(self.weights[i]) + "\nLayer {} Bias ".format(i) + str(self.biases[i].shape) + "\n" + str(self.biases[i]) + "\n"
-        return out[:-1]
+        for i in range(len(self.w)):
+            out += "Layer {} Weight ".format(i) + str(self.w[i].shape) + "\n" + str(
+                self.w[i]) + "\nLayer {} Bias ".format(i) + str(self.b[i].shape) + "\n" + str(self.b[i]) + "\n\n"
+        return out[:-2]    
+
+    def addDenseLayer(self, number_of_nodes, activation="relu"):
+        """
+        adds a fully connected layer into the network. Initialized with random weights.
+        """
+        if activation not in ["relu", "sigmoid"]:
+            raise Exception("Activation function is not supported: {}".format(activation))
+        self.activation += [activation]
+        self.w += [np.matrix(2 * np.random.random((number_of_nodes, self.w[-1].shape[0])) - 1)]
+        self.b += [np.matrix(2 * np.random.random((number_of_nodes, 1)) - 1)]
+
+    add = addDenseLayer
 
     def feedForward(self, data):
-        """
-        data must be a matrix  of shape (1, n) where n is the number of input values
-        returns an array of activation matricies at each layer
-        """
-        ls = len(self.weights)
-        a = [''] * ls
+        layers = len(self.w)
+        a = [""] * layers
         a[0] = data
-        for n in range(1, ls):
-            a[n] = self.g(a[n-1] * self.weights[n] + self.biases[n])
+        for n in range(1, layers):
+            z = self.w[n] * a[n-1] + self.b[n]
+            if self.activation[n] == "relu":
+                a[n] = self.relu(z)
+            elif self.activation[n] == "sigmoid":
+                a[n] = self.sig(z)             
         return a
-    
+
     ff = feedForward
-        
+
     def backPropogate(self, y, a):
         """
-        y is the expected output matrix
-        a is the list of activation matricies
+        y is the expected output
+        a is the array of activation values from feed forward
         """
-        #output layer is handled differently to hidden layers
-        error = y - a[-1]
-        gradient = np.multiply(error, self.dg(a[-1]))
-        delta = a[-2].T * gradient
-        self.weights[-1] += delta * self.alpha
-        self.biases[-1] += gradient * self.alpha
+        layers = len(self.w)
+        error = [""] * layers
+        gradient = [""] * layers
+        delta = [""] * layers
 
-        ls = len(self.weights)
-        for n in range(1, ls-1)[::-1]:
-            error = gradient * self.weights[n+1].T
-            gradient = np.multiply(error, self.dg(a[n]))
-            delta = a[n-1].T * gradient
-            self.weights[n] += delta * self.alpha
-            self.biases[n] += gradient * self.alpha
-            
-    bp = backPropogate
-
+        for n in range(1, layers)[::-1]:
+            #calculate error
+            if n == (layers - 1):
+                error[n] = y - a[-1] #if output layer
+            else:
+                error[n] = self.w[n+1].T * error[n+1]
+            #calculate gradient
+            if self.activation[n] == "relu":
+                gradient[n] = np.multiply(self.reluDeriv(a[n]), error[n]) * self.alpha
+            elif self.activation[n] == "sigmoid":
+                gradient[n] = np.multiply(self.sigDeriv(a[n]), error[n]) * self.alpha
+            #calculate delta
+            delta[n] = gradient[n] * a[n-1].T
+            #update weights/biases
+            self.w[n] += delta[n]
+            self.b[n] += gradient[n]
+        
     def train(self, train_x, train_y, epochs):
         """
         train_x is a matrix of training data where each row is a set of training inputs.
@@ -82,24 +88,17 @@ class NN:
         epochs is the number of times the network should train over the given training data.
         """
         for n in range(epochs):
-            y_error = []
+            y_error = [""] * len(train_x)
             activations = []
             for i in range(len(train_x)):
-                activations = self.feedForward(train_x[i])
-                y_error = train_y[i] - activations[-1]
-                self.backPropogate(train_y[i], activations)
-                
+                activations = self.feedForward(train_x[i].T)
+                y_error[i] = np.mean(np.abs(train_y[i].T - activations[-1]).T)
+                self.backPropogate(train_y[i].T, activations)
 
-            
-            #if (n == 1) or (n == epochs//4) or (n == 2*epochs//4) or (n == 3*epochs//4) or (n == epochs-1):
-            #if (n % epochs//4) == 0:
-            if (n % 250 == 0):
-                print("Training loss: " + str(np.mean(np.square(y_error))))
-                #print("Training loss: " + str(np.mean(np.abs(train_y[-1] - activations[-1]))))
-                
+            if (n % 100 == 0):
+                print("Training loss: " + str(np.round(y_error, 5)))
 
-
-#example training data to solve the XOR problem
+# example training data to solve the XOR problem
 
 train_x = np.matrix([
     [0, 0],
@@ -117,11 +116,10 @@ train_y = np.matrix([
 
 x = train_x
 y = train_y
-
+    
 nn = NN(2)
-nn.add(3)
-nn.add(4)
-nn.add(2)
+nn.add(128)
+nn.add(128)
+nn.add(2, "sigmoid")
 
-
-nn.train(x, y, 15000 +1 )
+nn.train(x, y, 500 + 1)
